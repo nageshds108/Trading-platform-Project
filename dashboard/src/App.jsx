a3cf5990aed5e37acab625b570bf552f1fe66b7d
Nagesh <nageshds0793@gmail.com>
2026-02-11 18:28:06 +0530

    fixes

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/*" element={<Dashboard />} />
+      <Route path="/*" element={<Home />} />
     </Routes>
   );
 };
